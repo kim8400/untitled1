8ec6aee73d6d892c60ff071c38cec9cf8a20a4ea
kim8400 <202230106@email.daelim.ac.kr>
2024-10-11 22:24:27 +0900

    week1.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, World!");
+    }
+}
+class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
